821c995d81313561e118607a08117b5ac9fcd013
joydeepta <joydeepta.dutta@gmail.com>
2026-09-11 04:51:25 +0000

    rectify alb

--- alb.tf
+++ alb.tf
@@ -1,25 +1,3 @@
-/*
-resource "aws_lb" "web_lb" {
-  name        = "${local.project_tags.Name}-web-alb"
-  internal           = false #this will be internet facing 
-  load_balancer_type = "application" #we are creating application lb
-  security_groups    = [aws_security_group.alb_sg.id]
-  subnets            = [for subnet in aws_subnet.web_subnet : subnet.id]
-
-  enable_deletion_protection = true
-
-  
-
-tags = {
-    Name = "${local.project_tags.Name}-web-sg"
-   
-  }
-}
-
-output "alb_dns_name" {
-  value = aws_lb.web_lb.dns_name
-}
-*/
 #we will create target group and attach instances behind tf 
 resource "aws_lb_target_group" "web_tg" {
   name        = "${local.project_tags.Name}-web-alb-tg"
